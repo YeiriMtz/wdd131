1585beadd737e9dd7b043c8c29a139b1cb5bd41a
YeiriMtz <yeirimartinez80@gmail.com>
2025-05-20 23:04:18 -0600

    updated

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,4 +5,10 @@ const modified = new Date(document.lastModified);
 const formatted = modified.toLocaleString();
 document.getElementById("lastModified").textContent = formatted;
 
-    
+// Toggle menu button
+const menuToggle = document.getElementById("menuToggle");
+const menuItems = document.getElementById("menuItems");
+
+menuToggle.addEventListener("click", () => {
+  menuItems.classList.toggle("show");
+});
