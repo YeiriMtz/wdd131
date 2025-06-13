d76c3975e2f68d8c2bf64da9c233ad1fb032aca4
YeiriMtz <yeirimartinez80@gmail.com>
2025-06-12 22:33:44 -0600

    home mobile version done

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -4,3 +4,10 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 const modified = new Date(document.lastModified);
 const formatted = modified.toLocaleString();
 document.getElementById("lastModified").textContent = formatted;
+// Hamburger menu toggle
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
